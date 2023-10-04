2a9ba9b73e95dd38712afc23c5ae5f8158d07c59
AstasiaG <anstasa.li@gmail.com>
2023-10-04 18:38:01 +0400

    fix: pixel perfect matching

--- index.js
+++ index.js
@@ -9,14 +9,20 @@ let windowWidth = document.documentElement.clientWidth;
 let sliderIndex = 0;
 let itemWidth = sliderItem[0].offsetWidth;
 
-if(windowWidth < 500) {
-  advantagesTitles[0].innerHTML = 'У нас самые выгодные<br>и низкие цены';
-  advantagesTitles[3].innerHTML = 'Мы являемся официальным партнером DELL';
-  advantagesTitles[3].nextElementSibling.innerHTML = 'Мы лучший официальный поставщик продукции DELL в России<br>и странах СНГ';
-} else if(windowWidth > 500) {
-  advantagesTitles[0].innerHTML = 'Самые выгодные и низкие цены';
-  advantagesTitles[3].innerHTML = 'Официальный партнер DELL';
-  advantagesTitles[3].nextElementSibling.innerHTML = 'Официальный поставщик продукции DELL в России и странах СНГ';
+
+window.addEventListener("resize", ChangeAdvantText);
+
+function ChangeAdvantText () {
+  if(windowWidth < 500) {
+    advantagesTitles[0].innerHTML = 'У нас самые выгодные<br>и низкие цены';
+    advantagesTitles[3].innerHTML = 'Мы являемся официальным партнером DELL';
+    advantagesTitles[3].nextElementSibling.innerHTML = 'Мы лучший официальный поставщик продукции DELL в России<br>и странах СНГ';
+  } 
+  if(windowWidth > 500) {
+    advantagesTitles[0].innerHTML = 'Самые выгодные и низкие цены';
+    advantagesTitles[3].innerHTML = 'Официальный партнер DELL';
+    advantagesTitles[3].nextElementSibling.innerHTML = 'Официальный поставщик продукции DELL в России и странах СНГ';
+  }
 }
 
 function SwipeSlider () {
@@ -28,8 +34,6 @@ function SwipeSlider () {
     dots[sliderIndex].className = 'dot active';
     sliderIndex ++;
 }
-
-setInterval(SwipeSlider, 4000);
 
 function dotsSwiper() {
   if(this.className.includes('active')) {
@@ -46,15 +50,36 @@ function dotsSwiper() {
   }
 }
 
+setInterval(SwipeSlider, 4000);
+
 for (let i = 0; i < dots.length; i++) {
   dots[i].addEventListener( "click" , dotsSwiper);
 }
+
 userIcon.addEventListener('click', () => {
   userMenu.classList.toggle('d-none');
+
+  burgerMenu.classList.remove('nav__list-active');
+  burgerMenu.closest('nav').classList.remove('active__bg');
+  burgerMenuBtn.classList.remove('burger-active');
 })
 
 burgerMenuBtn.addEventListener('click', () => {
   burgerMenu.classList.toggle('nav__list-active');
   burgerMenu.closest('nav').classList.toggle('active__bg');
   burgerMenuBtn.classList.toggle('burger-active');
+  userMenu.classList.add('d-none');
+})
+
+document.addEventListener('click', (el) => {
+  const burgerWind = el.composedPath().includes(burgerMenu);
+  const userWind = el.composedPath().includes(userMenu);
+  if(!burgerWind && !burgerMenuBtn) {
+    burgerMenu.classList.remove('nav__list-active');
+    burgerMenu.closest('nav').classList.remove('active__bg');
+    burgerMenuBtn.classList.remove('burger-active');
+  }
+  if (!userWind && !userIcon) {
+    userMenu.classList.add('d-none');
+  }
 })
